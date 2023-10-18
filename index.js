963cd5c678a74adc6da13d6fa1cd668aff6a17c9
JD <joedavies2500@gmail.com>
2023-10-18 12:14:06 +0100

    add pagepiling

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+$(document).ready(function () {
+  $("#pagepiling").pagepiling({
+    menu: null,
+    direction: "vertical",
+    verticalCentered: true,
+    sectionsColor: ["white", "#d9d9d9"],
+    scrollingSpeed: 700,
+    easing: "swing",
+    loopBottom: false,
+    loopTop: false,
+    css3: true,
+    navigation: {
+      textColor: "#000",
+      bulletsColor: "#353535",
+      position: "right",
+      tooltips: ["Landing", "About"],
+    },
+    normalScrollElements: null,
+    normalScrollElementTouchThreshold: 5,
+    touchSensitivity: 5,
+    keyboardScrolling: true,
+    sectionSelector: ".section",
+    animateAnchor: false,
+
+    //events
+    onLeave: function (index, nextIndex, direction) {},
+    afterLoad: function (anchorLink, index) {},
+    afterRender: function () {},
+  });
+});
+
 const blurDiv = document.querySelector(".blur-load");
 const img = blurDiv.querySelector("img");
 
